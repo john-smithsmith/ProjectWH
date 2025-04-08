6e756446275a778a441b8491745b47e18db9402f
조원후 <purplehaze0@naver.com>
2025-04-08 17:31:35 +0900

    Inventory
    인벤토리 추가

--- ProjectWh/material/Factory.cs
+++ ProjectWh/material/Factory.cs
@@ -13,7 +13,20 @@ namespace ProjectWh
             return new WoodSword();
         }
 
-        
+        public static Item CreateRustSword()
+        {
+            return new RustSword();
+        }
+
+        public static Item CreateIronSword()
+        {
+            return new IronSword();
+        }
+
+        public static Item CreateGold(int amount)
+        {
+            return new Gold(amount);
+        }
     }
 
     public static class MonsterFactory
@@ -23,6 +36,34 @@ namespace ProjectWh
             return new DummyScarecrow(name, hp, dropItem);
         }
 
+        // 1번 방 허수아비
+        public static Monster CreateCave01Scarecrow()
+        {
+            return new DummyScarecrow("연습용 허수아비", 1000, new Gold(50));
+        }
 
+        // 2번 방 허수아비
+        public static Monster CreateCave02Scarecrow()
+        {
+            return new DummyScarecrow("강한 허수아비", 2000, new Gold(50)); 
+        }
+
+        // 3번 방 허수아비
+        public static Monster CreateCave03Scarecrow()
+        {
+            return new DummyScarecrow("매우 강한 허수아비", 3000, new Gold(50)); 
+        }
+
+        // 4번 방 허수아비
+        public static Monster CreateCave04Scarecrow()
+        {
+            return new DummyScarecrow("엄청 매우 강한 허수아비", 4000, new Gold(50)); 
+        }
+
+        // 보스 허수아비
+        public static Monster CreateBossScarecrow()
+        {
+            return new DummyScarecrow("보스 허수아비", 5000, new Gold(50)); 
+        }
     }
 }
---
+++ ProjectWh/player/Inventory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectWh
+{
+    public class Inventory
+    {
+        public List<Item> Items { get; private set; }
+        public Inventory()
+        {
+            Items = new List<Item>();
+        }
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            Items.Remove(item);
+        }
+
+    }
+}
