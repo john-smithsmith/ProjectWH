8892982a87a1ca82a485a8fc97a6237bdac85e06
조원후 <purplehaze0@naver.com>
2025-04-08 17:36:27 +0900

    InventoryDisplay
    인벤토리 보이게 구현

--- ProjectWh/player/Inventory.cs
+++ ProjectWh/player/Inventory.cs
@@ -17,11 +17,29 @@ namespace ProjectWh
         public void AddItem(Item item)
         {
             Items.Add(item);
+            Console.WriteLine($"{item.Name}확득");
         }
 
         public void RemoveItem(Item item)
         {
             Items.Remove(item);
+            Console.WriteLine($"{item.Name}손실");
+        }
+
+        public void DisplayInventory()
+        {
+            Console.WriteLine("인벤토리:");
+            if (Items.Count > 0)
+            {
+                for (int i = 0; i < Items.Count; i++)
+                {
+                    Console.WriteLine($"- {Items[i].Name}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("인벤토리가 없음");
+            }
         }
 
     }
